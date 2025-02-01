b32ed8fe7a489e4dd7881940a689955d969d3f0f
Jonathan Duplantis <jonathan.duplantis@hotmail.com>
2025-02-01 14:38:41 -0500

    tests avec pygame_gui

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# Example file showing a circle moving on screen
-import pygame
+from buttons import *
+import pygame_gui
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1280
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+road_img = pygame.image.load("road.png")
+
+logo_scale = 0.3
+logo_img = pygame.image.load("logo.png")
+logo_img = pygame.transform.scale(logo_img, (logo_img.get_width() * logo_scale, logo_img.get_height() * logo_scale))
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -17,27 +24,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("gray")
+    pygame.draw.rect(screen, "black", (round(SCREEN_WIDTH/16), round(3 * SCREEN_HEIGHT/32), round(7 * SCREEN_WIDTH / 8), round(7 * SCREEN_HEIGHT / 8)))
+    
+    screen.blit(logo_img, (0, 0))
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
+    # Update Écran
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    # FPS CAPPING (60)
     dt = clock.tick(60) / 1000
 
 pygame.quit()
---
+++ pygame_GUI.py
@@ -0,0 +1,58 @@
+import pygame
+import pygame_gui
+
+pygame.init()
+
+pygame.display.set_caption('Quick Start')
+
+# Fenêtre et manager
+window_surface = pygame.display.set_mode((800, 600))
+background = pygame.Surface((800, 600))
+background.fill(pygame.Color('green'))
+
+# Charger le style du fichier JSON
+manager = pygame_gui.UIManager((800, 600), "styles.json")
+
+logo_img = pygame.image.load("road.png")
+
+# Création du bouton avec un style défini dans styles.json
+hello_button = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect((0, 0), (logo_img.get_width() + 20, logo_img.get_height() + 20)),
+    text="",  # On ne met pas de texte pour ne voir que l'image
+    manager=manager,
+    object_id=pygame_gui.core.ObjectID(class_id="@build_tiles"),  # Doit correspondre à l'ID dans styles.json
+    anchors={"center": "center"}
+)
+
+clock = pygame.time.Clock()
+is_running = True
+
+while is_running:
+    time_delta = clock.tick(60) / 1000.0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = False
+
+        # Vérifier si le bouton est cliqué
+        if event.type == pygame_gui.UI_BUTTON_PRESSED and event.ui_element == hello_button:
+            print("Bouton cliqué !")
+            if not hello_button.is_selected:
+                hello_button.select()
+                print("Selected")
+                continue
+            if hello_button.is_selected:
+                hello_button.unselect()
+                print("Unselected")
+                continue
+
+        manager.process_events(event)
+
+    manager.update(time_delta)
+
+    # Affichage
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+    pygame.display.update()
+
+pygame.quit()
